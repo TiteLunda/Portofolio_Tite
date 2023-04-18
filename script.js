d1f0f68493ee02c4ad5c5db4babcdff1ccdc0428
Tite Lunda <98431708+TiteLunda@users.noreply.github.com>
2023-04-18 00:47:11 -0700

    StyleLint Error Correction

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileLinks = document.querySelectorAll('.mobile-link');
 const toggleActiveClass = (element) => {
- element.classList.toggle('active');
+  element.classList.toggle('active');
 };
 const removeActiveClass = (element) => {
- element.classList.remove('active');
+  element.classList.remove('active');
 };
 
 hamburger.addEventListener('click', () => {
- toggleActiveClass(hamburger);
- toggleActiveClass(mobileMenu);
+  toggleActiveClass(hamburger);
+  toggleActiveClass(mobileMenu);
 });
 
 mobileLinks.forEach((link) => {
- link.addEventListener('click', () => {
-   removeActiveClass(hamburger);
-   removeActiveClass(mobileMenu);
- });
+  link.addEventListener('click', () => {
+    removeActiveClass(hamburger);
+    removeActiveClass(mobileMenu);
+  });
 });
